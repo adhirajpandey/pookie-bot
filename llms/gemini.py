6fc81950f42cf02cbe1d9d3b085fd9d26d0ade04
adhirajpandey <87516052+adhirajpandey@users.noreply.github.com>
2024-06-28 07:33:30 +0000

    add gemini llm

---
+++ llms/gemini.py
@@ -0,0 +1,23 @@
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+genai.configure(api_key=GEMINI_API_KEY)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+model_json = genai.GenerativeModel(
+    "gemini-1.5-flash", generation_config={"response_mime_type": "application/json"}
+)
+
+
+def gemini_call(query):
+    response = model.generate_content(query)
+    return response.text
+
+def gemini_call_json(query):
+    response = model_json.generate_content(query)
+    return response.text
